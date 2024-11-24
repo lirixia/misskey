81189b09785db3c92fe1621a9fae71057d4e9a3f
eternal-flame-AD <yume@yumechi.jp>
2024-11-24 13:51:29 -0600

    Feat: 読み込み時に systemd 風のログを出すように
    Signed-off-by: eternal-flame-AD <yume@yumechi.jp>
    
     [Cherry-Picked misskey-temp#a531ae56f3](https://git.0il.pw/n/misskey-temp/commit/a531ae56f3cb0d4206003840cd4dd85d58a7322c)

--- packages/frontend/src/components/global/MkLoading.vue
+++ packages/frontend/src/components/global/MkLoading.vue
@@ -4,13 +4,22 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<div :class="[$style.root, { [$style.inline]: inline, [$style.colored]: colored, [$style.mini]: mini, [$style.em]: em }]">
-	<div :class="$style.container">
-		<svg :class="[$style.spinner]" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
-			<circle :class="[$style.path]" cx="25" cy="25" r="20" fill="none" stroke-width="6px" style="fill: none; stroke: currentColor; stroke-width: 6px;"></circle>
-		</svg>
+	<div
+		:class="[$style.root, { [$style.inline]: inline, [$style.colored]: colored, [$style.mini]: mini, [$style.em]: em }]">
+		<div :class="$style.container">
+			<svg :class="[$style.spinner, $style.bg]" viewBox="0 0 168 168" xmlns="http://www.w3.org/2000/svg">
+				<g transform="matrix(1.125,0,0,1.125,12,12)">
+					<circle cx="64" cy="64" r="64" :class="[$style.bgcircle]" />
+				</g>
+			</svg>
+			<svg :class="[$style.spinner, $style.fg, { [$style.static]: static }]" viewBox="0 0 168 168"
+				xmlns="http://www.w3.org/2000/svg">
+				<g transform="matrix(1.125,0,0,1.125,12,12)">
+					<path d="M128,64C128,28.654 99.346,0 64,0C99.346,0 128,28.654 128,64Z" :class="[$style.fgpath]" />
+				</g>
+			</svg>
+		</div>
 	</div>
-</div>
 </template>
 
 <script lang="ts" setup>
@@ -38,6 +47,7 @@ const props = withDefaults(defineProps<{
 	0% {
 		transform: rotate(0deg);
 	}
+
 	100% {
 		transform: rotate(360deg);
 	}
@@ -48,10 +58,12 @@ const props = withDefaults(defineProps<{
 		stroke-dasharray: 1, 150;
 		stroke-dashoffset: 0;
 	}
+
 	50% {
 		stroke-dasharray: 90, 150;
 		stroke-dashoffset: -35;
 	}
+
 	100% {
 		stroke-dasharray: 90, 150;
 		stroke-dashoffset: -124;
@@ -108,5 +120,17 @@ const props = withDefaults(defineProps<{
 	stroke: var(--MI_THEME-accent);
 	stroke-linecap: round;
 	animation: dash 1.2s ease-in-out infinite;
+}
+
+.bgcircle {
+	fill: none;
+	stroke: currentColor;
+	stroke-width: 21.33px;
+}
+
+.fgpath {
+	fill: none;
+	stroke: currentColor;
+	stroke-width: 21.33px;
 }
 </style>
