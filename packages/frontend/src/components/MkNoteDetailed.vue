a654b4f6e865edf5ec63de6e33749aef916f6c07
Miry <miry@outlook.lv>
2025-01-05 21:15:56 +0900

    Fix: MkInstanceTickerの右横に表示されるユーザー名を削除

--- packages/frontend/src/components/MkNoteDetailed.vue
+++ packages/frontend/src/components/MkNoteDetailed.vue
@@ -115,15 +115,16 @@ SPDX-License-Identifier: AGPL-3.0-only
 						</div>
 						<MkInstanceTicker v-if="showTicker" :instance="appearNote.user.instance" @click="showOnRemote" />
 					</div>
+					<!--
 					<div :class="$style.noteHeaderUsernameAndBadgeRoles">
 						<div :class="$style.noteHeaderUsername">
-							<MkAcct :user="appearNote.user" />
+							<MkAcct :user="appearNote.user"/>
 						</div>
 						<div v-if="appearNote.user.badgeRoles" :class="$style.noteHeaderBadgeRoles">
-							<img v-for="(role, i) in appearNote.user.badgeRoles" :key="i" v-tooltip="role.name"
-								:class="$style.noteHeaderBadgeRole" :src="role.iconUrl!" />
+							<img v-for="(role, i) in appearNote.user.badgeRoles" :key="i" v-tooltip="role.name" :class="$style.noteHeaderBadgeRole" :src="role.iconUrl!"/>
 						</div>
 					</div>
+					-->
 				</div>
 			</header>
 			<div :class="$style.noteContent">
